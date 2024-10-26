f3c6571dcec12a52813d07394ed16ba3ef6ad6f5
Dazannah <beattius@gmail.com>
2024-10-26 16:28:53 +0200

    added split effect

--- asteroid.py
+++ asteroid.py
@@ -1,5 +1,8 @@
 import pygame
+import random
+
 from circleshape import CircleShape
+from constants import ASTEROID_MIN_RADIUS
 
 
 class Asteroid(CircleShape):
@@ -11,3 +14,22 @@ class Asteroid(CircleShape):
 
     def update(self, dt):
         self.position += self.velocity * dt
+
+    def split(self):
+        self.kill()
+
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
+
+        angle = random.uniform(20, 50)
+
+        new_vector_1 = self.velocity.rotate(angle)
+        new_vector_2 = self.velocity.rotate(-angle)
+
+        new_radius = self.radius - ASTEROID_MIN_RADIUS
+
+        asteroid1 = Asteroid(self.position.x, self.position.y, new_radius)
+        asteroid2 = Asteroid(self.position.x, self.position.y, new_radius)
+
+        asteroid1.velocity = new_vector_1 * 1.2
+        asteroid2.velocity = new_vector_2 * 1.2
